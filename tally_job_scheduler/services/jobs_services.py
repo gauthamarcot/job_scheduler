0b0a924ff76bfb200c9c5a6068d48d8c946eab09
gouthamarcot <gauthamarcot@yahoo.com>
2025-06-28 02:03:42 +0530

    untested jobservice [post]

--- tally_job_scheduler/services/jobs_services.py
+++ tally_job_scheduler/services/jobs_services.py
@@ -1,7 +1,25 @@
 from sqlmodel import Session
 
+from tally_job_scheduler.models.jobs import Job, JobDep
 from tally_job_scheduler.schema.job import JobSubmission
 
 
 def create_new_job(job: JobSubmission, session: Session):
-    pass
+
+    new_job = Job.from_orm(job)
+    new_job.status = "blocked" if job.depends_on else "pending"
+
+    session.add(new_job)
+    session.commit()
+    session.refresh(new_job)
+
+    if job.depends_on:
+        for dependency in job.depends_on:
+            dep_link = JobDep(
+                job_id=new_job.id,
+                depends_on_job_id=dependency
+            )
+            session.add(dep_link)
+        session.commit()
+    session.refresh(new_job)
+    return new_job
