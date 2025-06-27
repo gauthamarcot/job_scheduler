8c963af006e915670200005bf24ceb10315d2540
gouthamarcot <gauthamarcot@yahoo.com>
2025-06-28 00:54:37 +0530

    added health checks and db connections

--- tally_job_scheduler/main.py
+++ tally_job_scheduler/main.py
@@ -1,9 +1,38 @@
-from fastapi import FastAPI
-from routes import health_routes, jobs_routes
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.exc import OperationalError
+from sqlmodel import Session, select
 
+from routes import jobs_routes
+from utils import get_session
 app = FastAPI()
 
 
 app.include_router(jobs_routes.router)
-app.include_router(health_routes.router)
 
+
+@app.get("/health")
+async def health(session: Session = Depends(get_session)):
+    try:
+        session.exec(select(1)).one()
+        return {"api_status": "ok",
+                "db_status": "ok"}
+    except OperationalError as e:
+        raise HTTPException(
+            status_code=503,
+            detail={
+                "api_status": "ok",
+                "db_status": "error",
+                "error_message": "Could not connect to the database.",
+                "details": str(e)
+            }
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail={
+                "api_status": "error",
+                "db_status": "unknown",
+                "error_message": "An unexpected error occurred.",
+                "details": str(e)
+            }
+        )
---
+++ tally_job_scheduler/utils.py
@@ -0,0 +1,23 @@
+import os
+
+from sqlalchemy import Engine
+from sqlmodel import SQLModel, create_engine, Session
+
+DB_USER = os.getenv("POSTGRES_USER", "postgres")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "toor#toor")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5433")
+DB_NAME = os.getenv("POSTGRES_DB", "postgres")
+
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+def get_db() -> Engine:
+    engine = create_engine(DATABASE_URL)
+    return engine
+
+
+def get_session():
+    engine = get_db()
+    with Session(engine) as session:
+        yield session
